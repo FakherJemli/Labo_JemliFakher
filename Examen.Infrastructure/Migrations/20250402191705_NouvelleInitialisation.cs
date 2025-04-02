964ac598e447418089a657e33ab3ad4fafb0276b
FakherJemli <fakher.jemli@esprit.tn>
2025-04-02 20:32:49 +0100

    Ajoutez des fichiers projet.

---
+++ Examen.Infrastructure/Migrations/20250402191705_NouvelleInitialisation.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NouvelleInitialisation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdresseLabo",
+                table: "Laboratoires",
+                newName: "Localisation");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Localisation",
+                table: "Laboratoires",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Localisation",
+                table: "Laboratoires",
+                newName: "AdresseLabo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdresseLabo",
+                table: "Laboratoires",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
